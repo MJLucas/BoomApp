ac259e424e7eafde6b28814e74d638ac46ce9423
Mark Lucas <marklucas@Marks-MacBook-Pro.local>
2016-06-16 22:20:39 +0100

    BoomApp
    Source code for iOS app, BoomApp

---
+++ BoomApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BoomApp
+//
+//  Created by Mark Lucas on 06/05/2016.
+//  Copyright © 2016 Mark Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolBg: UIImageView!
+    @IBOutlet weak var uncoolButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        coolLogo.hidden = false
+        coolBg.hidden = false
+        uncoolButton.hidden = true
+    }
+
+}
+
